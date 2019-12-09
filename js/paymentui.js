9789ed60ef656cd8227521cc00a9ca7ff610bb48
Alice Frumin <alice@aghtrategies.com>
2019-12-09 16:07:10 -0500

    calculating totals

--- js/paymentui.js
+++ js/paymentui.js
@@ -7,21 +7,37 @@ CRM.$(function ($) {
 
   var calculateTotal = function () {
     var total = 0;
+    var subtotal = 0;
+    var pfee = 0;
+    var latefee = 0;
+    var partId = '';
+    var paymentAmount = 0;
+
     $.each($("input[name^='payment']"), function () {
-      var amt = $(this).val();
-      if ($.isNumeric(amt)) {
-        total = parseFloat(total) + parseFloat(amt);
+      partId = $(this).attr('id').substring(8);
+
+      // get late fees
+      if ($("input[name='latefee[" + partId + "]").val()) {
+        latefee = parseFloat($("input[name='latefee[" + partId + "]").val());
       }
+
+      if ($.isNumeric($(this).val())) {
+        paymentAmount = $(this).val();
+
+        // Calculate Processing Fees by Participant
+        pfee = (parseFloat(paymentAmount) * CRM.vars.paymentui.processingFee / 100).toFixed(2);
+      }
+      // calculate subtotals
+      subtotal = (parseFloat(paymentAmount) + parseFloat(pfee) + parseFloat(latefee)).toFixed(2);
+
+      // calculate total
+      total = (parseFloat(subtotal) + parseFloat(total)).toFixed(2);
+
+      $("input[name='subtotal[" + partId + "]").val(subtotal);
+      $("input[name='pfee[" + partId + "]").val(pfee);
+
     });
 
-    var creditcardfees = (total * CRM.vars.paymentui.processingFee / 100).toFixed(2);
-    var latefees = 0;
-    if (parseFloat($('#latefees').html()) > 0) {
-      latefees = $('#latefees').html();
-    }
-
-    document.getElementById('creditCardFees').innerHTML = creditcardfees;
-    total = (parseFloat(total) + parseFloat(creditcardfees) + parseFloat(latefees)).toFixed(2);
     document.getElementById('total').innerHTML = total;
   };
 
